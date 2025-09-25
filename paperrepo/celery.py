028cf193c459e379334dbec1550d2f4d6d48104f
denmark0128 <denmarkesmande8737@gmail.com>
2025-09-25 18:44:19 +0800

    fix

--- paperrepo/celery.py
+++ paperrepo/celery.py
@@ -1,9 +1,10 @@
 # project_root/celery.py
 import os
 #from celery import Celery
-
+'''
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paperrepo.settings')
 
 app = Celery('your_project')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+'''
