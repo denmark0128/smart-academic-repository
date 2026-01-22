de2f2b05c28726b1e2471a85580e630bdbe96dfe
denmark0128 <denmarkesmande8737@gmail.com>
2026-01-22 19:18:32 +0800

    added gunicorn config

---
+++ paperrepo/gunicorn.config.py
@@ -0,0 +1,15 @@
+# Server socket
+bind = "0.0.0.0:8000"
+backlog = 2048
+
+# Worker processes
+workers = 3
+worker_class = "sync"
+worker_connections = 1000
+timeout = 30
+keepalive = 2
+
+# Logging
+accesslog = "-"
+errorlog = "-"
+loglevel = "info"
